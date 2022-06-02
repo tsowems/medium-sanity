b370c0537a24b8203363d7da12e197ac61141ce5
tsowems <tsowems@gmail.com>
2022-06-02 15:34:23 +0100

    Home page with post from sanity

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,70 @@
-import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
+import Header from "../components/Header";
+import Link from 'next/link';
+import {sanityClient, urlFor} from '../sanity';
+import {Post} from "../typings";
 
-const Home: NextPage = () => {
+
+interface Props {
+  posts: [Post];
+}
+
+export default function Home({posts}: Props) {
+  console.log(posts)
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="max-w-7xl max-auto">
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
+        <title>Medium Blog</title>
       </Head>
+      <Header />
+      <div className="flex justify-between items-center bg-yellow-400 border-y
+       border-black py-10 lg:py0">
+        <div className="px-10 space-y-5">
+          <h1 className="text-6xl max-w-xl font-serif"><span className="underline decoration-black decoration-4">Medium</span> Stay curious.</h1>
+          <h2>Discover stories, thinking, and expertise from writers on any topic.</h2>        </div>
+        <div className="">
+          <img className="hidden md:inline-flex h-32 lg:h-full"
+           src="https://accountabilitylab.org/wp-content/uploads/2020/03/Medium-logo.png" alt="" />
+        </div>
+      </div>
 
-      <h1>This is Medium 2.0</h1>
-    </div>
-  )
-}
+      {/* post from sanity CMS */}
+      <div>
+      {posts.map((post) => (
+          <Link key={post._id} href={`/post/${post.slug.current}`}>
+            <div>
+              <h1>i am a post</h1>
+            </div>
+          </Link>
+        ))} 
+          </div>
+          </div>
+        );
+        }
+  
+export const getServerSideProps = async () => {
+  const query = `*[_type == "post"]{
+    _id,
+    title,
+    slug,
+    author -> {
+    name,
+    image
+  },
+  description,
+  mainImage,
+  slug
+  }
+  `;
 
-export default Home
+  const posts = await sanityClient.fetch(query);
+
+  return {
+    props: {
+      posts,
+    },
+  }
+
+};
