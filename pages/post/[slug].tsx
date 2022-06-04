40cbc16c6713f35effc87641fb25abac7a9ced4e
tsowems <tsowems@gmail.com>
2022-06-04 19:41:20 +0100

    final push

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -136,7 +136,7 @@ function Post({ post }: Props)  {
          <div className="flex flex-col p-10 my-10 max-w-2xl mx-auto shadow-yellow-500 shadow space-y-2">
              <h3 className="text-4xl" >Comments</h3>
              <hr />
-             {post.comments.map((comment: Comment) => (
+             {post.comments.map((comment) => (
                  <div key={comment._id}>
                      <p>
                          <span className="text-yellow-500">{comment.name}</span>
