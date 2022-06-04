19f315845699c2e991d5da4ab6afaacca057b4f4
tsowems <tsowems@gmail.com>
2022-06-04 19:34:19 +0100

    final version

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -114,7 +114,7 @@ function Post({ post }: Props)  {
             <label className="block mb-5">
                 <span className="text-gray-700">Comment</span>
                 <textarea 
-                {...register("comment", {required: true})}
+                {...register("comment", {required: true})} 
                 className="shadow border rounded py-2 px-3 form-textarea mt-1 block w-full ring-yellow-500 outline-none focus:ring"  placeholder="Comment" rows={8} />
             </label>
             <div className="flex flex-col p-5">
